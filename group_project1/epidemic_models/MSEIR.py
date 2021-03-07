32814be8e8dcbdf09aa4a8b462b18f4ca223fff7
huqiwen0313 <huqiwen0313@gmail.com>
2021-03-06 19:01:47 -0500

    update SEIR notebook

--- group_project1/epidemic_models/MSEIR.py
+++ group_project1/epidemic_models/MSEIR.py
@@ -4,8 +4,31 @@ from scipy.integrate import solve_ivp
 from scipy.integrate import odeint
 
 
-def sir(a, b, N):
-    return lambda t, y: np.array([-a/N*y[1]*y[0], a/N*y[1]*y[0]-b*y[1], b*y[1]])
+def sir(y, t, alpha, beta):
+    """SIR model for population fraction"""
+    s, i, r = y
+    dsdt = -alpha * i * s
+    didt = alpha * i * s - beta * i
+    drdt = beta * i
+    return [dsdt, didt, drdt]
+
+
+def sirSim(y0, t, alpha, beta, showPlot=True):
+    sirSoln = odeint(sir, y0, t, args=(alpha, beta))
+    s, i, r = sirSoln.T
+
+    # plot results
+    if showPlot:
+        plt.plot(t, s, label="S")
+        plt.plot(t, i, label="I")
+        plt.plot(t, r, label="R")
+        plt.legend()
+        plt.title('SIR Model')
+        plt.xlabel('Days')
+        plt.ylabel('Population')
+        plt.show()
+
+    return sirSoln
 
 
 def seir(y, t, alpha, beta, gamma):
@@ -17,19 +40,46 @@ def seir(y, t, alpha, beta, gamma):
     return [dsdt, dedt, didt, drdt]
 
 
+def seir_control(y, t, alpha, beta, gamma, c):
+    s, e, i, r = y
+    dsdt = -(1-c)*beta * s * i
+    dedt = (1-c)*beta * s * i - alpha * e
+    didt = alpha * e - gamma * i
+    drdt = gamma * i
+    return [dsdt, dedt, didt, drdt]
+
+
+def seir_control_dynamic(y, t, alpha, beta, gamma, cmax, tstar):
+    s, e, i, r = y
+    sl = cmax/tstar
+    c = min(cmax, sl * t)
+
+    dsdt = -(1-c)*beta * s * i
+    dedt = (1-c)*beta * s * i - alpha * e
+    didt = alpha * e - gamma * i
+    drdt = gamma * i
+    return [dsdt, dedt, didt, drdt]
+
 def mseir():
     return
 
 
-def seirSim(y0, beta, alpha, gamma, t, showPlot=True):
-    seirSoln = odeint(seir, y0, t, args=(alpha, beta, gamma))
+def seirSim(y0, beta, alpha, gamma, t, c=None,
+            showPlot=True, cmax=None, tstar=None, dynamic=False):
+    if not c:
+        seirSoln = odeint(seir, y0, t, args=(alpha, beta, gamma))
+    elif dynamic:
+        seirSoln = odeint(seir_control_dynamic, y0, t,
+                          args=(alpha, beta, gamma, cmax, tstar))
+    else:
+        seirSoln = odeint(seir_control, y0, t, args=(alpha, beta, gamma, c))
     s, e, i, r = seirSoln.T
 
     # plot results
     if showPlot:
         plt.plot(t, s, label="S")
-        plt.plot(t, e, label="I")
-        plt.plot(t, i, label="E")
+        plt.plot(t, e, label="E")
+        plt.plot(t, i, label="I")
         plt.plot(t, r, label="R")
         plt.legend()
         plt.title('SEIR Model')
